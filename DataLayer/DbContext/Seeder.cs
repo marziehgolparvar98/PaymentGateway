fdfbdf3a212d379b79ad3f70238018d5f2eba26a
Marzie Golparvar <mz.golparvar@MZ-GOLPARVAR-PR>
2022-04-09 16:36:28 +0430

    DataSeeder

---
+++ DataLayer/DbContext/Seeder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DbContext
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(this IApplicationBuilder appBuilder)
+        {
+            using (var scope = appBuilder.ApplicationServices.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+                using var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                using var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var user = new IdentityUser
+                {
+                    UserName = "Admin",
+                    Email = "mz.golparvar@ephoenix.net",
+                };
+                if (!roleManager.RoleExistsAsync("Admin").Result)
+                {
+                    if (!userManager.Users.Any(current => current.Email.ToLower() == "mz.golparvar@ephoenix.net".ToLower()))
+                    {
+                        userManager.CreateAsync(user, password: "P@ssw0rd").Wait();
+                    }
+                    roleManager.CreateAsync(new IdentityRole { Name = "Admin" }).Wait();
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+
+            };
+        }
+
+    }
+}
