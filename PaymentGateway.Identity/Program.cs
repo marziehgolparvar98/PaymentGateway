8f961e32e0f8ae6bdd7a796b89eb5cfdb9ca3530
Marzie Golparvar <mz.golparvar@MZ-GOLPARVAR-PR>
2022-04-09 13:36:13 +0430

    Startup, Jwt in AppSetting and conectionString in AppSetting

--- PaymentGateway.Identity/Program.cs
+++ PaymentGateway.Identity/Program.cs
@@ -1,10 +1,3 @@
-using DataLayer.DbContext;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using PaymentGateway.Provider.Interface;
-using PaymentGateway.Provider.Service;
-using System.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,13 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(option =>
-option.UseSqlServer("Data Source =. ;Initial Catalog =PaymentGateway ;Persist Security Info =True ;Integrated Security =True ;"));
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddScoped<IAuthenticateService, AuthenticateService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +14,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
